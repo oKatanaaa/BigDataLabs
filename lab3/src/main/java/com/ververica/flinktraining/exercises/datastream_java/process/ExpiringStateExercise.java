4adbc8d25fc246a3dc8304efa316edd4001e9b10
oKatanaaa <whitemarsstudios@gmail.com>
2021-12-19 18:32:40 +0400

    new: finished lab 3

---
+++ lab3/src/main/java/com/ververica/flinktraining/exercises/datastream_java/process/ExpiringStateExercise.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2017 data Artisans GmbH, 2019 Ververica GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ververica.flinktraining.exercises.datastream_java.process;
+
+import com.ververica.flinktraining.exercises.datastream_java.datatypes.TaxiFare;
+import com.ververica.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
+import com.ververica.flinktraining.exercises.datastream_java.sources.TaxiFareSource;
+import com.ververica.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
+import com.ververica.flinktraining.exercises.datastream_java.utils.ExerciseBase;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.KeyedCoProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+import java.io.IOException;
+
+/**
+ * The "Expiring State" exercise from the Flink training
+ * (http://training.ververica.com).
+ *
+ * The goal for this exercise is to enrich TaxiRides with fare information.
+ *
+ * Parameters:
+ * -rides path-to-input-file
+ * -fares path-to-input-file
+ *
+ */
+public class ExpiringStateExercise extends ExerciseBase {
+	static final OutputTag<TaxiRide> unmatchedRides = new OutputTag<TaxiRide>("unmatchedRides") {};
+	static final OutputTag<TaxiFare> unmatchedFares = new OutputTag<TaxiFare>("unmatchedFares") {};
+
+	public static void main(String[] args) throws Exception {
+		ParameterTool params = ParameterTool.fromArgs(args);
+		final String ridesFile = params.get("rides", ExerciseBase.pathToRideData);
+		final String faresFile = params.get("fares", ExerciseBase.pathToFareData);
+
+		final int maxEventDelay = 60;           // events are out of order by max 60 seconds
+		final int servingSpeedFactor = 600; 	// 10 minutes worth of events are served every second
+
+		// set up streaming execution environment
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+		env.setParallelism(ExerciseBase.parallelism);
+
+		DataStream<TaxiRide> rides = env
+				.addSource(rideSourceOrTest(new TaxiRideSource(ridesFile, maxEventDelay, servingSpeedFactor)))
+				.filter((TaxiRide ride) -> (ride.isStart && (ride.rideId % 1000 != 0)))
+				.keyBy(ride -> ride.rideId);
+
+		DataStream<TaxiFare> fares = env
+				.addSource(fareSourceOrTest(new TaxiFareSource(faresFile, maxEventDelay, servingSpeedFactor)))
+				.keyBy(fare -> fare.rideId);
+
+		SingleOutputStreamOperator<Tuple2<TaxiRide, TaxiFare>> processed = rides
+				.connect(fares)
+				.process(new EnrichmentFunction());
+
+		printOrTest(processed.getSideOutput(unmatchedFares));
+
+		env.execute("ExpiringStateSolution (java)");
+	}
+
+	public static class EnrichmentFunction extends KeyedCoProcessFunction<Long, TaxiRide, TaxiFare, Tuple2<TaxiRide, TaxiFare>> {
+		private ValueState<TaxiRide> rideState;
+		private ValueState<TaxiFare> fareState;
+		private ValueState<Long> registeredTime;
+
+		@Override
+		public void open(Configuration config) {
+			rideState = getRuntimeContext().getState(new ValueStateDescriptor<>("saved ride", TaxiRide.class));
+			fareState = getRuntimeContext().getState(new ValueStateDescriptor<>("saved fare", TaxiFare.class));
+			registeredTime = getRuntimeContext().getState(new ValueStateDescriptor<>("registered" +
+					"timer marker", Long.class));
+		}
+
+		@Override
+		public void onTimer(long timestamp, OnTimerContext ctx, Collector<Tuple2<TaxiRide, TaxiFare>> out) throws Exception {
+			if (fareState.value() != null) {
+				ctx.output(unmatchedFares, fareState.value());
+				fareState.clear();
+			}
+			if (rideState.value() != null) {
+				ctx.output(unmatchedRides, rideState.value());
+				rideState.clear();
+			}
+		}
+
+		@Override
+		public void processElement1(TaxiRide ride, Context context, Collector<Tuple2<TaxiRide, TaxiFare>> out) throws Exception {
+			rideState.update(ride);
+			registerTimer(context, ride.getEventTime());
+			updateCollector(out);
+		}
+
+		@Override
+		public void processElement2(TaxiFare fare, Context context, Collector<Tuple2<TaxiRide, TaxiFare>> out) throws Exception {
+			fareState.update(fare);
+			registerTimer(context, fare.getEventTime());
+			updateCollector(out);
+		}
+
+		// Updated the collector only if both states contain values
+		private void updateCollector(Collector<Tuple2<TaxiRide, TaxiFare>> out) throws IOException {
+			TaxiFare fare = fareState.value();
+			TaxiRide ride = rideState.value();
+			if (fare == null || ride == null)
+				return;
+
+			out.collect(new Tuple2(ride, fare));
+			fareState.clear();
+			rideState.clear();
+			registeredTime.clear();
+		}
+
+		private void registerTimer(Context ctx, long time) throws IOException {
+			Long registeredTime = this.registeredTime.value();
+
+			if (registeredTime == null) {
+				ctx.timerService().registerEventTimeTimer(time);
+				this.registeredTime.update(time);
+			}
+			else {
+				ctx.timerService().deleteEventTimeTimer(registeredTime);
+			}
+		}
+	}
+
+
+}
